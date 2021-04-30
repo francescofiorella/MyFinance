8abaf6c85caa3444ccfe070835bd2001a820fb08
francesco_fiorella <francescofio99@gmail.com>
2021-05-01 01:38:43 +0200

    splash screen fixed

--- app/src/main/java/com/frafio/myfinance/ui/home/SplashScreenActivity.kt
+++ app/src/main/java/com/frafio/myfinance/ui/home/SplashScreenActivity.kt
@@ -1,19 +1,26 @@
 package com.frafio.myfinance.ui.home
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat
 import com.frafio.myfinance.R
 import com.frafio.myfinance.ui.auth.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.icon_bg)
+
         val fAuth = FirebaseAuth.getInstance()
 
         Handler(Looper.getMainLooper()).postDelayed({
@@ -25,7 +32,7 @@ class SplashScreenActivity : AppCompatActivity() {
             }
 
             val activityOptionsCompat = ActivityOptionsCompat
-                .makeCustomAnimation(applicationContext, android.R.anim.fade_in, android.R.anim.fade_out)
+                    .makeCustomAnimation(applicationContext, android.R.anim.fade_in, android.R.anim.fade_out)
             val intent = Intent(applicationContext, firstActivity)
             startActivity(intent, activityOptionsCompat.toBundle())
             finish()
