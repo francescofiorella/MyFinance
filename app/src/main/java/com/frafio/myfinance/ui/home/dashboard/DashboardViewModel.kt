87e04d8d1f23cd6183a90466df70e50ce28b96f2
francesco.fiorella <francescofio99@gmail.com>
2021-05-07 09:28:24 +0200

    stats optimized

--- app/src/main/java/com/frafio/myfinance/ui/home/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/frafio/myfinance/ui/home/dashboard/DashboardViewModel.kt
@@ -6,14 +6,15 @@ import com.frafio.myfinance.data.repositories.DataRepository
 class DashboardViewModel(
     repository: DataRepository
 ) : ViewModel() {
-    val dayAvgString: String = repository.calculateStats()[0]
-    val monthAvgString: String = repository.calculateStats()[1]
-    val todayTotString: String = repository.calculateStats()[2]
-    val totString: String = repository.calculateStats()[3]
-    val numTotString: String = repository.calculateStats()[4]
-    val ticketTotString: String = repository.calculateStats()[5]
-    val trenTotString: String = repository.calculateStats()[6]
-    val amTotString: String = repository.calculateStats()[7]
+    val stats = repository.calculateStats()
+    val dayAvgString = stats[0]
+    val monthAvgString = stats[1]
+    val todayTotString = stats[2]
+    val totString: String = stats[3]
+    val numTotString: String = stats[4]
+    val ticketTotString: String = stats[5]
+    val trenTotString: String = stats[6]
+    val amTotString: String = stats[7]
 
     val warningVisibility = repository.dashboardWarningVisibility()
     val statsVisibility = repository.dashboardStatsVisibility()
