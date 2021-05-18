ea7eeea3ea1f709d00a1cbd8853539f75950266f
francesco.fiorella <francescofio99@gmail.com>
2021-05-18 15:45:09 +0200

    viewUtils changes

--- app/src/main/java/com/frafio/myfinance/util/ViewUtils.kt
+++ app/src/main/java/com/frafio/myfinance/util/ViewUtils.kt
@@ -1,6 +1,5 @@
 package com.frafio.myfinance.util
 
-import android.annotation.SuppressLint
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
@@ -9,26 +8,19 @@ import com.frafio.myfinance.R
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
-fun View.snackbar(message: String) {
+fun View.snackbar(message: String, anchor: View? = null) {
     val nunito = ResourcesCompat.getFont(context, R.font.nunito)
 
     val snackbar = Snackbar.make(this, message, BaseTransientBottomBar.LENGTH_SHORT)
         .setBackgroundTint(ContextCompat.getColor(context, R.color.snackbar_bg))
         .setTextColor(ContextCompat.getColor(context, R.color.inverted_primary_text))
+
+    anchor?.let{
+        snackbar.setAnchorView(anchor)
+    }
+
     val tv = snackbar.view.findViewById<TextView>(R.id.snackbar_text)
     tv.typeface = nunito
-    snackbar.show()
-}
 
-@SuppressLint("ShowToast")
-fun View.snackbar(message: String, anchor: View) {
-    val nunito = ResourcesCompat.getFont(context, R.font.nunito)
-
-    val snackbar = Snackbar.make(this, message, BaseTransientBottomBar.LENGTH_SHORT)
-        .setAnchorView(anchor)
-        .setBackgroundTint(ContextCompat.getColor(context, R.color.snackbar_bg))
-        .setTextColor(ContextCompat.getColor(context, R.color.inverted_primary_text))
-    val tv = snackbar.view.findViewById<TextView>(R.id.snackbar_text)
-    tv.typeface = nunito
     snackbar.show()
 }
