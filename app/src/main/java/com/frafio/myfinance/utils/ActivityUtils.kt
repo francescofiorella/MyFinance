d1337050d31048cb45a0dc83ad258f7293bd6809
francesco_fiorella <francescofio99@gmail.com>
2022-02-12 12:19:19 +0100

    version updated

--- app/src/main/java/com/frafio/myfinance/utils/ActivityUtils.kt
+++ app/src/main/java/com/frafio/myfinance/utils/ActivityUtils.kt
@@ -1,9 +1,6 @@
 package com.frafio.myfinance.utils
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
