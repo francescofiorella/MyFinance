76f147e965239d7461e8705a74fd1ae90816eb04
Francesco Fiorella <francescofio99@gmail.com>
2024-08-05 20:32:10 +0200

    category selection layout changed for sw600dp

--- app/src/main/java/com/frafio/myfinance/ui/home/payments/PaymentsFragment.kt
+++ app/src/main/java/com/frafio/myfinance/ui/home/payments/PaymentsFragment.kt
@@ -303,7 +303,13 @@ class PaymentsFragment : BaseFragment(), PurchaseInteractionListener, PaymentLis
 
         if (fromCategoryIcon) {
             layout.findViewById<LinearLayout>(R.id.editPurchaseLayout).visibility = View.GONE
-            layout.findViewById<ConstraintLayout>(R.id.editCategoryLayout).visibility = View.VISIBLE
+            if (resources.getBoolean(R.bool.is600dp)) {
+                layout.findViewById<LinearLayout>(R.id.editCategoryLayout)
+                    .visibility = View.VISIBLE
+            } else {
+                layout.findViewById<ConstraintLayout>(R.id.editCategoryLayout)
+                    .visibility = View.VISIBLE
+            }
             layout.findViewById<LinearLayout>(R.id.housing_layout).setOnClickListener {
                 viewModel.updateCategory(purchase, DbPurchases.CATEGORIES.HOUSING.value)
                 dismissFun()
@@ -346,7 +352,13 @@ class PaymentsFragment : BaseFragment(), PurchaseInteractionListener, PaymentLis
         val editLayout = layout.findViewById<LinearLayout>(R.id.edit_layout)
         val deleteLayout = layout.findViewById<LinearLayout>(R.id.delete_layout)
         layout.findViewById<LinearLayout>(R.id.editPurchaseLayout).visibility = View.VISIBLE
-        layout.findViewById<ConstraintLayout>(R.id.editCategoryLayout).visibility = View.GONE
+        if (resources.getBoolean(R.bool.is600dp)) {
+            layout.findViewById<LinearLayout>(R.id.editCategoryLayout)
+                .visibility = View.GONE
+        } else {
+            layout.findViewById<ConstraintLayout>(R.id.editCategoryLayout)
+                .visibility = View.GONE
+        }
         editLayout.setOnClickListener {
             Intent(context, AddActivity::class.java).also {
                 it.putExtra(AddActivity.REQUEST_CODE_KEY, AddActivity.REQUEST_EDIT_CODE)
