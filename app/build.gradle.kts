f7e08d778841c24b5b0c2fcbcd369f3cb2ac4f9d
Francesco Fiorella <francescofio99@gmail.com>
2024-09-19 00:25:21 +0200

    textviews with drawable alignment fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 4
-        versionName = "4.6.10"
+        versionName = "4.6.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -68,8 +68,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.5")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.fragment:fragment-ktx:1.8.3")
 
@@ -98,8 +98,8 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
 
     // Jetpack Compose
-    implementation("androidx.compose.ui:ui:1.7.1")
+    implementation("androidx.compose.ui:ui:1.7.2")
     implementation("androidx.compose.material3:material3:1.3.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.2")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 }
