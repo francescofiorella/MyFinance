defacf12aa35f405986daff9d6715ba61c78be28
francescofiorella <francescofio99@gmail.com>
2026-09-21 09:30:27 +0200

    version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.frafio.myfinance"
         minSdk = 29
         targetSdk = 37
-        versionCode = 5000002
-        versionName = "5.0.2"
+        versionCode = 5000003
+        versionName = "5.0.3"
 
         vectorDrawables {
             useSupportLibrary = true
